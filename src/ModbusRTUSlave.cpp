37e1eb46d5b73b256cad724c17f01a75b2640eb2
wsl editor <cybergrany404@gmail.com>
2026-09-02 19:13:51 +0100

    Preserve queued Modbus RTU requests

--- src/ModbusRTUSlave.cpp
+++ src/ModbusRTUSlave.cpp
@@ -1,5 +1,30 @@
 #include "ModbusRTUSlave.h"
 
+namespace {
+
+uint16_t expectedRequestLength(const uint8_t* rtu, uint16_t receivedLen) {
+  if (receivedLen < 2) return 0;
+  switch (rtu[1]) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      return 8U;
+    case 15:
+    case 16: {
+      if (receivedLen < 7) return 0;
+      const uint16_t expectedLen = 9U + rtu[6];
+      return expectedLen <= 256U ? expectedLen : 0;
+    }
+    default:
+      return 0;
+  }
+}
+
+} // namespace
+
 ModbusRTUSlave::ModbusRTUSlave(Stream& serial, int dePin, int rePin) : _rtuComm(serial, dePin, rePin) {
   
 }
@@ -15,7 +40,7 @@ void ModbusRTUSlave::begin(uint8_t localUnitId, unsigned long baud, uint32_t con
 
 bool ModbusRTUSlave::poll() {
   ModbusADU adu;
-  ModbusRTUCommError error = _rtuComm.readAdu(adu);
+  ModbusRTUCommError error = _rtuComm.readAdu(adu, expectedRequestLength);
   if (error) return false;
   uint8_t unitId = adu.getUnitId();
   if (unitId != _localUnitId && unitId != 0) return false;
